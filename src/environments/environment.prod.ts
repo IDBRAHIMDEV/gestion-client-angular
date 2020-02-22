6428f4c94a5dd0b8a17dcb952b88ae4dcbd672bb
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2020-02-22 13:08:31 +0100

    client create form

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCcYIbc6Vb2KjzmcfOnfW7IRKzDivK3stk",
+    authDomain: "gestion-client-d81d1.firebaseapp.com",
+    databaseURL: "https://gestion-client-d81d1.firebaseio.com",
+    projectId: "gestion-client-d81d1",
+    storageBucket: "gestion-client-d81d1.appspot.com",
+    messagingSenderId: "776988168777",
+    appId: "1:776988168777:web:355daa4f7fd5337f1a6598"
+  }
 };
